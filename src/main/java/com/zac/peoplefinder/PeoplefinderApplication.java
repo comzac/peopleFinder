7a59448a9f0c41e430ba08ead11a272cbecd1fd2
LEESUNSOO <tjstndi77@naver.com>
2022-05-01 22:47:51 +0900

    first upload

---
+++ src/main/java/com/zac/peoplefinder/PeoplefinderApplication.java
@@ -0,0 +1,12 @@
+package com.zac.peoplefinder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PeoplefinderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PeoplefinderApplication.class, args);
+	}
+}
---
+++ src/test/java/com/zac/peoplefinder/PeoplefinderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zac.peoplefinder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PeoplefinderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
